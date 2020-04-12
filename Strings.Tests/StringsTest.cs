b07f7354cdbcc1046771ea33e507667afdec74fa
Frederick-S <mao_xiaodan@hotmail.com>
2020-04-12 19:38:33 +0800

    test: add more unit test

--- Strings.Tests/StringsTest.cs
+++ Strings.Tests/StringsTest.cs
@@ -5,6 +5,12 @@ namespace Strings.Tests
     public class StringsTest
     {
         [Fact]
+        public void ShouldReturnNull()
+        {
+            Assert.Null(Strings.ConvertToLowerCaseAndUnderscore(null));
+        }
+
+        [Fact]
         public void ShouldConvertToLowerCaseAndUnderscore()
         {
             Assert.Equal("hello_world", Strings.ConvertToLowerCaseAndUnderscore("Hello World"));
